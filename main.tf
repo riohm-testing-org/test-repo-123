03bc7ea38139e2a7249742f2b1152eab3c1f1b22
riohm <96946073+riohm@users.noreply.github.com>
2026-01-20 21:06:20 -0500

    Add file

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-rg"
+  location = "East US"
+
+  tags = {
+    Environment = "Test"
+  }
+}
