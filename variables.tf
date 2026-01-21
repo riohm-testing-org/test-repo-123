50662348c2ab578820e3e62e9b38b8e7b80945f3
riohm <96946073+riohm@users.noreply.github.com>
2026-01-20 21:34:49 -0500

    Add variables.tf for module inputs

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
